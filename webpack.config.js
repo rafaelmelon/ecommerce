89ad0d63bf5e87522504f537fea2bd9d8ecde6b6
Rafael Melón <info@rafaelmelon.es>
2017-08-08 11:18:59 +0200

    End configuration -- babel & webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const webpack = require('webpack')
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   devtool: 'cheap-module-eval-source-map',
