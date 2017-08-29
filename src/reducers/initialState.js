28f48e3d756b0e0d9880750829210494efd5ec60
Rafael Melón <info@rafaelmelon.es>
2017-08-29 20:22:54 +0200

    InitialState new file

---
+++ src/reducers/initialState.js
@@ -0,0 +1,18 @@
+const initialState = {
+  productList: {
+    products: [],
+    error: null,
+    loading: false
+  },
+  activeProduct: {
+    product: null,
+    error: null,
+    loading: false
+  },
+  cart: {
+    items: [],
+    total: 0
+  }
+}
+
+export default initialState
