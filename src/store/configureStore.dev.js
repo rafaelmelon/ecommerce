e0aa366d849f4066533d17bbfc2d4b2e0074c612
Rafael Melón <rafael.melon@opinno.com>
2017-09-20 14:47:12 +0200

    Init component product list

--- src/store/configureStore.dev.js
+++ src/store/configureStore.dev.js
@@ -1,12 +1,12 @@
 import { createStore, applyMiddleware } from 'redux'
-import logger from 'redux-logger'
+import { createLogger } from 'redux-logger'
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
 
 import rootReducer from '../reducers'
 
 const enhacer = composeWithDevTools(
-  applyMiddleware(thunk, logger())
+  applyMiddleware(thunk, createLogger())
 )
 
 export default function configureStore (initialState) {
--- webpack.config.js
+++ webpack.config.js
@@ -73,7 +73,7 @@ module.exports = {
   devServer: {
     host: '0.0.0.0',
     hot: true,
-    port: 8080,
+    port: 8081,
     inline: true,
     contentBase: path.join(__dirname, 'src'),
     historyApiFallback: true
